75484c6494689117baa3983c357f8e3c8bff81e9
Darya Kylosova <kylosovadaria@icloud.com>
2022-08-30 19:44:27 +0300

    pereezd na novuy compucter

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -6,16 +6,25 @@ import ArrowRight from "./ArrowRight";
 import ArrowLeft from "./ArrowLeft";
 
 const Carousel = () => {
+  const [slideClasses, setSlideClasses] = useState([classes.slide]);
+  const [activeSlides, setActiveSlides] = useState(slides.slice(0, 4));
+
+  const clickNext = () => {
+    setSlideClasses([...slideClasses, classes.active]);
+  };
+
+  console.log(slideClasses);
+  console.log(activeSlides);
   return (
     <div className={classes.carousel}>
       <div className={classes.wrapper}>
         <ArrowLeft className={classes.arrowLeft} />
-        {slides.map((item) => (
+        {activeSlides.map((item) => (
           <div key={Math.random() + Date.now()}>
-            <CarouselItem item={item} className={classes.slide} />
+            <CarouselItem item={item} className={slideClasses.join(" ")} />
           </div>
         ))}
-        <ArrowRight className={classes.arrowRight} />
+        <ArrowRight className={classes.arrowRight} onClick={clickNext} />
       </div>
     </div>
   );
